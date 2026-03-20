f5767a491859ec8566fb129f660b525bf91fab0e
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2026-03-20 14:53:26 +0530

    Added : String built-in functions like upper(), lower(), title(), strip(), split(), replace(), join(), find(), startswith(), endswith(), isupper(), islower().

---
+++ String_Handling/built-in_methods.py
@@ -0,0 +1,32 @@
+print("Ankur Sinsinwar".lower())
+
+print("Ankur Sinsinwar".upper())
+
+print("ankur sinsinwar".title())
+
+print("  Ankur Sinsinwar  ".strip())
+
+print("Ankur Sinsinwar".replace("Ankur", "Devrath"))
+
+print("ban bat bag".split())
+
+print("ban,bat,bag".split(","))
+
+print(" | ".join(['ban', 'bat', 'bag']))
+
+print("Ankur Sinsinwar".find("Sinsinwar"))
+
+print("Ankur Sinsinwar".find("Devrath") )
+
+print("Ankur Sinsinwar".startswith("Ankur") )
+
+print("Ankur Sinsinwar".endswith("Sinsinwar"))
+
+print("Ankur".isupper())
+
+print("ANKUR".isupper())
+
+print("Ankur".islower())
+
+print("ankur".islower())
+
